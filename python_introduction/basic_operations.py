273f7cc0eb90100485ca1ee2f2132cf97935a06a
LILAD <lidetadmassu217@gmail.com>
2025-02-05 15:43:28 +0300

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+dif= number1 - number2
+multi = number1 * number2
+print(f"Addition of {number1} and {number2} is  {sum}")
+print(f"Subtraction of {number1} and {number2} is {dif}")
+print(f"Multiplication of {number1} and {number2} is {multi}")
