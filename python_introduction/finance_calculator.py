d573b69ec09291e1343ff77104dc4c115b046212
Lidet Admassu <146084647+lA12-coder@users.noreply.github.com>
2025-07-10 00:31:21 +0300

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,6 @@
-mon_income = int(input("Enter your monthly income: "))
-mon_exp = int(input("Enter your total monthly expenses: "))
-mon_saving = mon_income - mon_exp
-R  = 0.05
-Projected_saving  = mon_saving * 12 + (mon_saving * 12 * R)
-print(f"Your monthly savings are ${mon_saving}.")
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+Projected_saving=monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"Your monthly savings are ${monthly_savings}.")
 print(f"Projected saving after one year, with interest, is: ${int(Projected_saving)}.")
