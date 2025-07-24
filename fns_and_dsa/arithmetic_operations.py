90f4d655436906a762514b732a608411fc26a3b5
Lidet <lidetadmassu217@gmail.com>
2025-07-24 16:35:41 +0300

    Initial Commit

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1:float,num2:float,operation:str):
+    match operation:
+        case "add":
+            return num1+num2
+        case "subtract":
+             return num1-num2
+        case "multiply":
+             return num1*num2
+        case "divide":
+             return num1/num2 if num2!=0 else "Division By zero is not Possible"
+        case _:
+            return "Enter the correct operation among (add, subtract, multiply, divide)"
+             
+
+
+    
