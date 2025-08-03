4239719debc7b8e6433dfe882050445f3b7755c9
Lidet <lidetadmassu217@gmail.com>
2025-08-03 22:56:19 +0300

    Initial Commit

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,38 @@
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+        self.__is_checked_out = False
+    def set_check_out_value(self,value):
+        self.__is_checked_out = value
+    def get_check_out_value(self):
+        return self.__is_checked_out
+        
+
+class Library:
+    def __init__(self):
+        self.__books = []
+        
+    def add_book(self,book) -> None:
+        self.__books.append(book)
+    def check_out_book(self,title) -> None:
+        for book in self.__books:
+            if book.title == title:
+                book.set_check_out_value(True)
+                break                
+            
+    def return_book(self,title) -> None:
+        for book in self.__books:
+            if book.title == title:
+                if book.get_check_out_value()== True:
+                    book.set_check_out_value(False)
+                    break
+                
+                
+    def list_available_books(self):
+        for book in self.__books:
+            if book.get_check_out_value() == False:
+                print(
+                    f"{book.title} by {book.author}"
+                )
+                
