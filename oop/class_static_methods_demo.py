f264869d3b0d17bd70a41a4e02d556071c9fb51b
Lidet <lidetadmassu217@gmail.com>
2025-08-09 22:20:24 +0300

    Initial commit

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -5,7 +5,7 @@ class Calculator:
     def add(a,b):
         return a + b
     @classmethod
-    def multiply(cls ,a ,b):
+    def multiply(cls, a ,b):
         print(f"Calculation type: {cls.calculation_type}")
         return a * b
     
