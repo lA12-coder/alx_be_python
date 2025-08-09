5cbb25f542ea26b4126a3e15b1cad3399751477b
Lidet <lidetadmassu217@gmail.com>
2025-08-09 18:44:45 +0300

    Initial commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,17 +2,24 @@ class Book:
     def __init__(self,title:str,author:str) -> None:
         self.title = title
         self.author = author
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
     
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size:int) -> None:
         super().__init__(title, author)
         self.file_size = file_size
-        
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+    
+    
 class PrintBook(Book):
     def __init__(self, title: str, author: str,page_count:int) -> None:
         super().__init__(title, author)
         self.page_count = page_count
-        
+    
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
@@ -23,11 +30,5 @@ class Library:
         self.books.append(book)
     def list_books(self):
         for book in self.books:
-            match type(book).__name__:
-                case "Book":
-                    print(f"Book: {book.title} by {book.author}")
-                case "EBook":
-                    print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-                case "PrintBook":
-                    print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            print(book)
                     
